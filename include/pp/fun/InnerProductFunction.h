a5dcc678d26cc98e94dfac795bf8e2cd1a9c7987
Jeff Kilpatrick <random-g@bouncingsheep.org>
2018-06-10 17:01:49 -0700

    Inner product implements row-based interface

--- include/pp/fun/InnerProductFunction.h
+++ include/pp/fun/InnerProductFunction.h
@@ -11,6 +11,7 @@ namespace pp {
         InnerProductFunction(IFunctionPtr arg0, IFunctionPtr arg1);
 
         Color Eval(float x, float y) const override;
+        void EvalRow(const std::vector<float>& xs, float y, Color* out) const override;
         const std::string& GetName() const override;
     };
 
--- src/pp/fun/InnerProductFunction.cc
+++ src/pp/fun/InnerProductFunction.cc
@@ -19,6 +19,25 @@ pp::Color InnerProductFunction::Eval(float x, float y) const
     return Color{ ip };
 }
 
+void InnerProductFunction::EvalRow(const std::vector<float>& xs, float y, Color* out) const
+{
+    m_buffers.at(0).resize(xs.size(), Color{0.f});
+    m_buffers.at(1).resize(xs.size(), Color{0.f});
+
+    GetArgs().at(0)->EvalRow(xs, y, m_buffers.at(0).data());
+    GetArgs().at(1)->EvalRow(xs, y, m_buffers.at(1).data());
+
+    for (size_t i = 0; i < xs.size(); ++i)
+    {
+        *out = Color{
+            m_buffers.at(0)[i].C1 * m_buffers.at(1)[i].C1
+          + m_buffers.at(0)[i].C2 * m_buffers.at(1)[i].C2
+          + m_buffers.at(0)[i].C3 * m_buffers.at(1)[i].C3
+        };
+        ++out;
+    }
+}
+
 const std::string& InnerProductFunction::GetName() const
 {
     return InnerProductFunctionGenerator{}.GetName();
