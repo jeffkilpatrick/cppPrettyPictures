521df485cb90987b635166585e781e5f7cdc77d3
Jeff Kilpatrick <random-g@bouncingsheep.org>
2018-07-10 19:24:28 -0700

    Render asynchronously
    I have no idea how to use these APIs. Maybe this is correct.

--- src/pretty-screen/PrettyScreenView.h
+++ src/pretty-screen/PrettyScreenView.h
@@ -2,9 +2,24 @@
 
 #include "pp/fun/Registry.h"
 
+@interface RenderOperation : NSOperation {
+    pp::Registry* registry;
+    NSSize size;
+    size_t maxDepth;
+}
+
+@property NSImage* image;
+
+- (instancetype)initWithRegistry:(pp::Registry*)registry size:(NSSize)size maxDepth:(size_t)maxDepth;
+@end
+
+
 @interface PrettyScreenView : ScreenSaverView {
-    @private pp::Registry registry;
-    @private size_t maxDepth;
+    @private
+    pp::Registry registry;
+    size_t maxDepth;
+    NSOperationQueue* renderQueue;
+    RenderOperation* renderer;
 }
 
 @end
--- src/pretty-screen/PrettyScreenView.mm
+++ src/pretty-screen/PrettyScreenView.mm
@@ -6,7 +6,50 @@
 #include "pp/expr/Eval.h"
 #include "pp/expr/Generate.h"
 
+@implementation RenderOperation
+
+@synthesize image;
+
+- (instancetype)initWithRegistry:(pp::Registry*)r size:(NSSize)s maxDepth:(size_t)maxD;
+{
+    self = [super init];
+
+    if (self) {
+        image = nil;
+        registry = r;
+        size = s;
+        maxDepth = maxD;
+    }
+
+    return self;
+}
+
+-(void)main {
+    // Create a random image expression
+    auto expr = RandomExpression(*self->registry, self->maxDepth);
+
+    // Turn the expression into pixel values
+    auto ppimage = Eval(*expr, size.width, size.height);
+
+    // Turn the image into a PNG. BMP would be faster, but the PNG
+    // library more easily supports getting the encoded data without
+    // creating a temporary file.
+    auto pngData = pp::WritePng(ppimage);
+
+    // Turn the PNG into an NSImage
+    auto nsdata = [NSData dataWithBytes:pngData.data() length:pngData.size()];
+    image = [[NSImage alloc] initWithData:nsdata];
+}
+
+@end
+
+#pragma mark ------------------------------------------------------------------
+
 @implementation PrettyScreenView
+
+-(RenderOperation*)makeRenderer {
+    return [[RenderOperation alloc] initWithRegistry:&registry size:[self bounds].size maxDepth:maxDepth];
+}
 
 - (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
 {
@@ -14,31 +57,25 @@
     if (self) {
         [self setAnimationTimeInterval:3.0];
         self->maxDepth = 5;
+        self->renderQueue = [[NSOperationQueue alloc] init];
+        self->renderer = nil;
     }
     return self;
 }
 
 - (void)animateOneFrame
 {
-    auto size = [self bounds].size;
+    if (!renderer) {
+        renderer = [self makeRenderer];
+        [renderQueue addOperation:renderer];
+    }
 
-    // Create a random image expression
-    auto expr = RandomExpression(self->registry, self->maxDepth);
+    [renderer waitUntilFinished];
 
-    // Turn the expression into pixel values
-    auto image = Eval(*expr, size.width, size.height);
+    [renderer.image drawInRect:[self bounds]];
 
-    // Turn the image into a PNG. BMP would be faster, but the PNG
-    // library more easily supports getting the encoded data without
-    // creating a temporary file.
-    auto pngData = pp::WritePng(image);
-
-    // Turn the PNG into an NSImage
-    auto nsdata = [NSData dataWithBytes:pngData.data() length:pngData.size()];
-    auto nsimage = [[NSImage alloc] initWithData:nsdata];
-
-    // Draw the image
-    [nsimage drawInRect:[self bounds]];
+    renderer = [self makeRenderer];
+    [renderQueue addOperation:renderer];
 }
 
 - (BOOL)hasConfigureSheet
