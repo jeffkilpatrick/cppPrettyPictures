df4990b1d5f85896f88ee89ecff399c40fc3f053
jeffkilpatrick <random-g@bouncingsheep.org>
2018-08-06 15:00:44 -0700

    Migrate from bazel to xcodebuild (#4)
    == Resources
    * https://www.cocoawithlove.com/2010/02/custom-build-rules-generated-tables-and.html
    * https://help.apple.com/xcode/mac/10.0/#/itcaec37c2a6
    * https://github.com/mattstevens/xcode-googletest
    * https://github.com/google/googletest/blob/master/googletest/docs/XcodeGuide.md
    
    == Changes
    * Convert all non-test targets to Xcode
    * Drop support for libbmp
    * Migrate gtest suite with external test executable
    * Delete bazel files

---
+++ pp/include/pp/Export.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "pp/utility/Exports.h"
+
+#include <string>
+
+namespace pp {
+    class Image;
+
+    /**
+        Export the given image to PNG.
+    **/
+    PP_EXPORT void WritePng(const std::string& path, const pp::Image& img, const std::string& description);
+    PP_EXPORT std::vector<uint8_t> WritePng(const pp::Image& img, const std::string& description);
+}
